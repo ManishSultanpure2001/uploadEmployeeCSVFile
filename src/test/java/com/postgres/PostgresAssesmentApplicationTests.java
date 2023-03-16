c69aa2efb69e1cd95ceec68aa53ad5468615a32b
ManishSultanpure2001 <manish.s@webkorps.com>
2023-03-16 16:29:14 +0530

    First Commit

--- src/test/java/com/postgres/PostgresAssesmentApplicationTests.java
+++ src/test/java/com/postgres/PostgresAssesmentApplicationTests.java
@@ -1,13 +1,32 @@
 package com.postgres;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PostgresAssesmentApplicationTests {
+import com.postgres.programs.TestPrograms;
+import com.postgres.serviceImpl.SaveEmployeeDetailsImpl;
 
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+class PostgresAssesmentApplicationTests {
+private static final int EXPECTDVALUE = 4;
+@Autowired
+private SaveEmployeeDetailsImpl service;
 	@Test
-	void contextLoads() {
+	void testPositiveMax() {
+		assertEquals(EXPECTDVALUE, TestPrograms.maxValue(new int[] {1,2,3,4}));
 	}
 
+    @Test  
+    public void testNegitiveMax(){  
+        System.out.println("test case cube");  
+        assertEquals(-1,TestPrograms.maxValue(new int[] {-1,-2,-3,-4}));  
+    }  
+	
+ 
 }
