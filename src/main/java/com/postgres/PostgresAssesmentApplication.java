5c1986118577c2b38926242b481e1691f46baafc
ManishSultanpure2001 <manish.s@webkorps.com>
2023-03-16 16:37:49 +0530

    Commit 1

--- src/main/java/com/postgres/PostgresAssesmentApplication.java
+++ src/main/java/com/postgres/PostgresAssesmentApplication.java
@@ -1,44 +1,14 @@
 package com.postgres;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.postgres.programs.TestPrograms;
-
-import lombok.experimental.var;
-import com.postgres.programs.*;
 @SpringBootApplication
 public class PostgresAssesmentApplication {
- 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PostgresAssesmentApplication.class, args);
 		
-	List<Integer> list=Arrays.asList(19,21,4,243,24,23);
-	
-	List<String> stringList =Arrays.asList("ramji","rafasj","raf1asj");
-	
-	String string = stringList.stream().reduce((a,b)->a.length()>b.length()?a:b).get();
-	System.out.println(string);
-//	TestPrograms.removeDuplicate();
-//	TestPrograms.firstNonRepeatingCharecter();
-//	TestPrograms.removeGivenCharecter();
-//	TestPrograms.stringInterMethod();
-//	TestPrograms.checkOperatorSize();
-//	TestPrograms.varArgsExtract(12,32,12,312);
-//	TestPrograms.stringSort();
-//	TestPrograms.sortStringInDecedingOrder();
-//	TestPrograms.sortStringOnTheBasisOFLastCahrecter();
-//	TestPrograms.sortStringOnTheBasisOFLastCahrecterIfSameThanSecondLast();
-//	TestPrograms.listCheck();
-//	TestPrograms.arraySort();
-	//TestPrograms.concurrentCheck();
-	//System.out.println(TestPrograms.validation("abc uhasd auifsds  auidbas auid "));
-	System.out.println(TestPrograms.removeDuplicate1());
 	}
 
 }
--- src/test/java/com/postgres/PostgresAssesmentApplicationTests.java
+++ src/test/java/com/postgres/PostgresAssesmentApplicationTests.java
@@ -1,32 +1,13 @@
 package com.postgres;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.postgres.programs.TestPrograms;
-import com.postgres.serviceImpl.SaveEmployeeDetailsImpl;
-
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootTest
-@Slf4j
 class PostgresAssesmentApplicationTests {
-private static final int EXPECTDVALUE = 4;
-@Autowired
-private SaveEmployeeDetailsImpl service;
+
 	@Test
-	void testPositiveMax() {
-		assertEquals(EXPECTDVALUE, TestPrograms.maxValue(new int[] {1,2,3,4}));
+	void contextLoads() {
 	}
 
-    @Test  
-    public void testNegitiveMax(){  
-        System.out.println("test case cube");  
-        assertEquals(-1,TestPrograms.maxValue(new int[] {-1,-2,-3,-4}));  
-    }  
-	
- 
 }
